42ce3b6559ee30674687f496466a962fa8f1aebf
KamiKozy <kamikozytpp@gmail.com>
2018-10-23 01:23:25 -0400

    attempt to fix heroku errors

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,7 @@ var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.all("tables.burgers", function(res) {
       cb(res);
     });
   },
